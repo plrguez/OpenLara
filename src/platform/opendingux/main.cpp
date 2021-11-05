6d9d0636ddf65649d406cbc1eca86ecf8b5f99f1
Pedro Luis Rodríguez González <pl.rguez@gmail.com>
2021-11-05 13:48:56 +0100

    Set window to RGB565

--- src/platform/opendingux/main.cpp
+++ src/platform/opendingux/main.cpp
@@ -596,7 +596,10 @@ int main(int argc, char **argv) {
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 6);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     /* In GLES, start in fullscreen mode using the vide mode currently in use. */
